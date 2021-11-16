b5e376f90bfb82db57b4b6d53e601574f02af09d
bajegbo joseph <damilolaj23@gmail.com>
2021-11-16 13:42:22 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "../index.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="flex-container">
+          <div className="logo-container">
+            <a href="#" className="logo"><span>send</span>FREIGHT </a>
+          </div>
+          <nav className="nav">
+            <form>
+              <input type="text" id="search-input" placeholder="Search" />
+              <button type="submit"> <i className="fa fa-search"></i> </button>
+            </form>
+            <div className="action-btns">
+              <button className="request"><a href="#">Request Quote</a></button>
+              <button className="book"><a href="#">Book Shipment</a></button>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      <i>b</i>
+      <div>
+        <h1>New Booking</h1>
+        <p>Fill in the information below to get a quote or create a new booking</p>
+      </div>
+      <form>
+        <div>
+          <h3>Select a service</h3>
+          <div>
+            <button>
+              Air Freight
+              <i></i>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Main
